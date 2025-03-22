32460d2badbdb80f5212b807e1dbb2c44363e488
sahinbilisim <shnblsm@gmail.com>
2025-03-22 14:55:14 +0300

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,15 @@
 function isEnoughCapacity(products, containerSize) {
-  let totalAmount = 0;
-  const product = { apples: 2, grapes: 3, carrots: 1 };
-  for (const product in products) {
-    totalAmount += products[product];
-  }
-  return totalAmount <= containerSize;
+  const totalQuantity = Object.values(products).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
+  return totalQuantity <= containerSize;
 }
+
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+
 console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+
 console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
